46632c002e3cd4b79bd0146fd8c4d0e32d9cce45
urutear <keitadance0208@gmail.com>
2025-02-18 05:36:11 +0900

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import nltk
-nltk.download('punkt_tab')
-
-tokenizer = nltk.tokenize.punkt.PunktLanguageVars()
-words = "I can't believe it's 2025!"
-
-print(tokenizer.word_tokenize(words))
-print(nltk.word_tokenize(words))
